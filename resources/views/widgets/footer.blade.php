bf4db725c28981e031d6dd51cd4d777873f1ee70
johnsonjuvenary <72694952+johnsonjuvenary@users.noreply.github.com>
2021-06-27 21:12:10 +0300

    first commit

---
+++ resources/views/widgets/footer.blade.php
@@ -0,0 +1,11 @@
+<div class="footer">
+    <div class="row">
+        <div class="col-sm-6">
+        </div>
+        <div class="text-right col-sm-6">
+            Tuchati © @php
+            echo date('Y');
+            @endphp All rights reserved.
+        </div>
+    </div>
+</div>
