1332bf66814926d501fc8f6364581b4a9d04b8ed
johnsonjuvenary <72694952+johnsonjuvenary@users.noreply.github.com>
2021-06-29 20:02:03 +0300

    Update guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -13,10 +13,10 @@
 
     <!-- Styles -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-    <link href="{{ secure_asset('plugins/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
-    <link href="{{ secure_asset('plugins/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ secure_asset('plugins/bootstrap/css/bootstrap-theme.min.css') }}" rel="stylesheet">
-    <link href="{{ secure_asset('css/guest.css') }}" rel="stylesheet">
+    <link href="plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="plugins/bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
+    <link href="css/guest.css" rel="stylesheet">
 </head>
 
 <body>
@@ -25,7 +25,7 @@
         <div class="row">
             <div class="col-md-6">
                 <a href="{{ url('/') }}" class="logo">
-                    <img src="{{ secure_asset('images/guest_logo.png') }}" alt="" />
+                    <img src="images/guest_logo.png" alt="" />
                 </a>
             </div>
 
@@ -57,8 +57,8 @@
         @include('widgets.footer')
     </div>
     <!-- Scripts -->
-    <script src="{{ secure_asset('plugins/jquery/jquery-3.2.1.min.js') }}"></script>
-    <script src="{{ secure_asset('plugins/bootstrap/js/bootstrap.min.js') }}"></script>
+    <script src="plugins/jquery/jquery-3.2.1.min.js"></script>
+    <script src="plugins/bootstrap/js/bootstrap.min.js"></script>
 </body>
 
 </html>
