76a89d92a8c2197c70eb15bbad96d7fe819862c0
johnsonjuvenary <72694952+johnsonjuvenary@users.noreply.github.com>
2021-06-30 19:03:18 +0300

    Update count.blade.php

--- resources/views/groups/widgets/count.blade.php
+++ resources/views/groups/widgets/count.blade.php
@@ -1,13 +1,13 @@
 <div class="count_widget">
     <div class="row">
         <div class="col-xs-6">
-            <a class="green" href="{{ url('/group/'.$group->id.'/stats') }}">
+            <a class="green" href="/group/'.$group->id.'/stats'">
                 {{ $group->countPeople($city->id) }}
             </a>
             {{ $city->name }}
         </div>
         <div class="col-xs-6">
-            <a class="blue" href="{{ url('/group/'.$group->id.'/stats') }}">
+            <a class="blue" href="'/group/'.$group->id.'/stats'">
                 {{ $group->countPeople() }}
             </a>
             Total
